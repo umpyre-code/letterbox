5e26e4d76d3d7767d8ad672e645c410818a27ca4
Brenden Matthews <brenden@diddyinc.com>
2019-06-21 14:47:02 -0400

    Fix tslint warning.

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,12 +2,12 @@ import { routerMiddleware } from 'connected-react-router'
 import { History } from 'history'
 import { applyMiddleware, createStore, Store } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import createSagaMiddleware from 'redux-saga'
+import reduxSaga from 'redux-saga'
 import { ApplicationState, createRootReducer, rootSaga } from './store'
 
 export default function configureStore(history: History): Store<ApplicationState> {
   const composeEnhancers = composeWithDevTools({})
-  const sagaMiddleware = createSagaMiddleware()
+  const sagaMiddleware = reduxSaga()
   const initialState = {}
 
   const store = createStore(
