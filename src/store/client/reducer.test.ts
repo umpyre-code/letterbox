21ddb27ce11acc30292ffb364b3440da9e2ca9c5
Brenden Matthews <brenden@diddyinc.com>
2019-07-30 19:15:23 -0400

    Reorder values.

--- src/store/client/reducer.test.ts
+++ src/store/client/reducer.test.ts
@@ -8,27 +8,27 @@ test('new_client_reducer', () => {
       ready: true
     },
     {
-      type: '@@client/SUBMIT_NEW_CLIENT_SUCCESS',
       payload: {
         client_id: '648ce922c2794b34b4f11d146b68dad2',
         jwt: {
+          secret: 'SG1uF5fNJmIatw0lOFqULiEUHUbUP7NLmaJvnGkg5LjMpxhlBP',
           token:
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw',
-          secret: 'SG1uF5fNJmIatw0lOFqULiEUHUbUP7NLmaJvnGkg5LjMpxhlBP'
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw'
         }
-      }
+      },
+      type: '@@client/SUBMIT_NEW_CLIENT_SUCCESS'
     }
   )
   expect(state).toEqual({
-    loading: false,
-    ready: true,
     credentials: {
       client_id: '648ce922c2794b34b4f11d146b68dad2',
       jwt: {
+        secret: 'SG1uF5fNJmIatw0lOFqULiEUHUbUP7NLmaJvnGkg5LjMpxhlBP',
         token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw',
-        secret: 'SG1uF5fNJmIatw0lOFqULiEUHUbUP7NLmaJvnGkg5LjMpxhlBP'
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw'
       }
-    }
+    },
+    loading: false,
+    ready: true
   })
 })
