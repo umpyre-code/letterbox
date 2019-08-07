9170c4f89af96e873d1a9cb7cb29fa89fbe906db
Brenden Matthews <brenden@diddyinc.com>
2019-08-07 19:05:24 -0400

    Actually fix the test.

--- src/store/client/reducer.test.ts
+++ src/store/client/reducer.test.ts
@@ -21,6 +21,7 @@ test('new_client_reducer', () => {
     }
   )
   expect(state).toEqual({
+    clientReady: false,
     credentials: {
       client_id: '648ce922c2794b34b4f11d146b68dad2',
       jwt: {
@@ -29,7 +30,7 @@ test('new_client_reducer', () => {
           'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw'
       }
     },
-    credentialsReady: true,
+    credentialsReady: false,
     loading: false
   })
 })
