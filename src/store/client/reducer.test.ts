2e2e5a614fe828755cbda727064a7cae545ec08a
Brenden Matthews <brenden@diddyinc.com>
2019-08-12 10:21:20 -0400

    Fix tests.

--- src/store/client/reducer.test.ts
+++ src/store/client/reducer.test.ts
@@ -4,9 +4,12 @@ test('new_client_reducer', () => {
   let state
   state = reducer(
     {
+      clientLoading: false,
       clientReady: false,
+      credentialsLoading: false,
       credentialsReady: false,
-      loading: true
+      phoneVerifying: false,
+      reload: false
     },
     {
       payload: {
@@ -21,6 +24,7 @@ test('new_client_reducer', () => {
     }
   )
   expect(state).toEqual({
+    clientLoading: false,
     clientReady: false,
     credentials: {
       client_id: '648ce922c2794b34b4f11d146b68dad2',
@@ -30,7 +34,9 @@ test('new_client_reducer', () => {
           'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU5NTM2ODEsImlhdCI6MTU2NDQxNzY4MSwiaXNzIjoiVW1weXJlIiwianRpIjoiOGQxZmY2ZjE3YmIxNDEwYWFhOGFjNWQ5ZTk1OGE5OWYiLCJuYmYiOjE1NjQ0MTc5ODEsInN1YiI6IjY0OGNlOTIyYzI3OTRiMzRiNGYxMWQxNDZiNjhkYWQyIn0.m3WKTr_5fraEbqcaen6W9-zL57kGT3O42T1XKfji4Sw'
       }
     },
+    credentialsLoading: false,
     credentialsReady: false,
-    loading: false
+    phoneVerifying: false,
+    reload: false
   })
 })
