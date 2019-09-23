91ea90ce92de5caa03e06392246b8936ca3b2a92
Brenden Matthews <brenden@diddyinc.com>
2019-09-23 18:07:25 -0400

    Compress on upload.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -x
 
 yarn install
 yarn build
-gsutil -m rsync -r -x index.html build gs://umpyre.com/
+gsutil -m rsync -r -x index.html -z html,css,js,txt build gs://umpyre.com/
 gsutil -m rsync -r build gs://umpyre.com/
