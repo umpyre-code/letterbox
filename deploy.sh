311610ab13590bd506756def5cd9c8b94a2f002f
Brenden Matthews <brenden@diddyinc.com>
2019-09-23 18:17:40 -0400

    Use cp instead of rsync.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ set -x
 
 yarn install
 yarn build
-gsutil -m rsync -r -x index.html -z html,css,js,txt build gs://umpyre.com/
-gsutil -m rsync -r build gs://umpyre.com/
+gsutil -m cp -r -z html,css,js,txt build gs://umpyre.com/
