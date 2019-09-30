f25ce5ef0615261dace3ea0472ab9ceb1b0f2e61
Brenden Matthews <brenden@diddyinc.com>
2019-09-30 10:44:06 -0400

    Update build script.

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ set -x
 rm -rf build
 yarn install
 yarn build
+gsutil -m cp -r -z html,css,js,map,mjs,txt,woff,woff2,svg,json build/static/* gs://umpyre.com/static/
+yarn run react-snap
 gsutil -m cp -r -z html,css,js,map,mjs,txt,woff,woff2,svg,json build/* gs://umpyre.com/
