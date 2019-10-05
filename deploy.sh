69c0eb002fc106b263446b50b2b0618fb223aa39
Brenden Matthews <brenden@diddyinc.com>
2019-10-05 10:33:06 -0400

    Fix react snap build.

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,6 @@ yarn install
 yarn build
 gsutil -m cp -r -z html,css,js,map,mjs,txt,woff,woff2,svg,json build/static/* gs://umpyre.com/static/
 yarn run react-snap
+# remove root index.html
+rm build/index.html
 gsutil -m cp -r -z html,css,js,map,mjs,txt,woff,woff2,svg,json build/* gs://umpyre.com/
